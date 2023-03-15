1bd97cc97c51cae1691ccaa60b9a0e30e21c8ed4
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-03-15 15:31:18 -0300

    feat: create cart context

---
+++ context/CartContext.js
@@ -0,0 +1,90 @@
+import { useState, createContext } from 'react';
+import { productsArray, getProductData } from '../productsData';
+
+export const CartContext = createContext({
+  items: [],
+  getProductQuantity: () => {},
+  addOneToCart: () => {},
+  removeOneFromCart: () => {},
+  deleteFromCart: () => {},
+  getTotalCost: () => {},
+});
+
+function CartProvider({ children }) {
+  const [cartProducts, setCartProducts] = useState([]);
+
+  function getProductQuantity(id) {
+    const quantity = cartProducts.find(
+      (product) => product.id === id
+    )?.quantity;
+
+    if (quantity === undefined) {
+      return 0;
+    }
+
+    return quantity;
+  }
+
+  function addOneToCart(id) {
+    const quantity = getProductQuantity(id);
+
+    if (quantity === 0) {
+      setCartProducts([...cartProducts, { id: id, quantity: 1 }]);
+    } else {
+      setCartProducts(
+        cartProducts.map((product) =>
+          product.id === id ? { ...product, quantity: quantity + 1 } : product
+        )
+      );
+    }
+  }
+
+  function removeOneFromCart(id) {
+    const quantity = getProductQuantity(id);
+
+    if (quantity > 0) {
+      deleteFromCart(id);
+    } else {
+      setCartProducts(
+        cartProducts.map((product) =>
+          product.id === id ? { ...product, quantity: quantity - 1 } : product
+        )
+      );
+    }
+  }
+
+  function deleteFromCart(id) {
+    setCartProducts((cartProducts) =>
+      cartProducts.filter((currentProduct) => {
+        return currentProduct.id !== id;
+      })
+    );
+  }
+
+  function getTotalCost() {
+    let total;
+
+    cartProducts.map((cartItem) => {
+      const productdata = getProductData(cartItem.id);
+
+      total += productdata.price * cartItem.quantity;
+    });
+
+    return total;
+  }
+
+  const contextValue = {
+    items: cartProducts,
+    getProductQuantity,
+    addOneToCart,
+    removeOneFromCart,
+    deleteFromCart,
+    getTotalCost,
+  };
+
+  return (
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+  );
+}
+
+export default CartProvider;
